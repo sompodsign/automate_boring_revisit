603ac12cb7dc9f4ead03262f668c2f5aa7f52138
Shampad Sharkar <sompodsign@gmail.com>
2020-10-10 02:38:25 +0600

    mcb added

---
+++ ch9/mcb.py
@@ -0,0 +1,8 @@
+# mcb.pyw - Saves and loads pieces of text to the clipboard.
+# Usage: py.exe mcb.pyw save <keyword> - saves clipboard to keyword.
+#        py.exe mcb.pyw <keyword> - loads keyword to clipboard.
+#        py.exe mcb.pyw list - Loads all keywords to clipboard.
+
+import shelve, pyperclip, sys
+mcbShelf = shelve.open('mcb')
+
